35fc546d31e925842e0afa0a1d14e0066b183829
Sander Dijkman <sdijkman2008@gmail.com>
2019-11-08 19:51:06 +0100

    doses bijna aan cocktail gekoppeld

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :cocktails do
+    resources :doses, only: [:index, :new, :create]
+  end
+  resources :doses, only: [:show, :edit, :update]
+
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+# puts 'Cleaning database...'
+# Cocktail.destroy_all
+puts 'Creating list of cocktails'
+
+url_cocktail = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+file_cocktail = open(url_cocktail).read
+hash_cocktail = JSON.parse(file_cocktail)
+array_cocktail = hash_cocktail.flatten[1].map { |hash| hash['strDrink'] }
+array_cocktail.each do |cocktail|
+  Cocktail.create(name: cocktail)
+end
+
+puts 'Finished cocktails!'
+
+puts 'Creating list of ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file_ingredient = open(url).read
+hash_ingredient = JSON.parse(file_ingredient)
+array_ingredient = hash_ingredient.flatten[1].map { |hash| hash['strIngredient1'] }
+array_ingredient.each do |ingredient|
+  Ingredient.create(name: ingredient)
+end
+
+puts 'Finished ingredients!'
+
+puts 'Creating list of dosages'
+
+one_cl = Dose.new(description: '1cl')
+one_cl.save!
+
+two_cl = Dose.new(description: '2cl')
+two_cl.save!
+
+three_cl = Dose.new(description: '3cl')
+three_cl.save!
+
+four_cl = Dose.new(description: '4cl')
+four_cl.save!
+
+five_cl = Dose.new(description: '5cl')
+five_cl.save!
+
+one_part = Dose.new(description: '1 part')
+one_part.save!
+
+two_part = Dose.new(description: '2 part')
+two_part.save!
+
+three_part = Dose.new(description: '3 part')
+three_part.save!
+
+handfull = Dose.new(description: 'A handful')
+handfull.save!
+
+around = Dose.new(description: 'Around the rim')
+around.save!
+
+puts 'Finished dosages!'
